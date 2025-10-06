114a493d5785e41bf1275d5612c3854ce7a621e4
Caleb <iamtechnerd@gmail.com>
2025-10-06 01:43:28 -0400

    Finished

--- src/main/java/com/jogwheel/tasks/services/impl/TaskListServiceImpl.java
+++ src/main/java/com/jogwheel/tasks/services/impl/TaskListServiceImpl.java
@@ -4,8 +4,13 @@ import com.jogwheel.tasks.domain.entities.TaskList;
 import com.jogwheel.tasks.repositories.TaskListRepository;
 import com.jogwheel.tasks.services.TaskListService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class TaskListServiceImpl implements TaskListService {
@@ -19,5 +24,54 @@ public class TaskListServiceImpl implements TaskListService {
     @Override
     public List<TaskList> listTaskLists() {
         return taskListRepository.findAll();
+    }
+
+    @Override
+    public TaskList createTaskList(TaskList taskList) {
+        if(taskList.getId() != null){
+            throw new IllegalArgumentException("Task list already has an ID!");
+        }
+        if(taskList.getTitle() == null || taskList.getTitle().isBlank()){
+            throw new IllegalArgumentException("Task list title must be present!");
+        }
+        LocalDateTime now = LocalDateTime.now();
+        taskListRepository.save(new TaskList(
+                null,
+                taskList.getTitle(),
+                taskList.getDescription(),
+                null,
+                now,
+                now
+
+        ));
+
+        return null;
+    }
+
+    @Override
+    public Optional<TaskList> getTaskList(UUID id) {
+        return taskListRepository.findById(id);
+    }
+
+    @Transactional
+    @Override
+    public TaskList updateTaskList(UUID taskListId, TaskList taskList) {
+        if(taskList.getId() == null){
+            throw new IllegalArgumentException("Task list must have ID");
+        }
+        if(!Objects.equals(taskList.getId(), taskListId)){
+            throw new IllegalArgumentException("Attempting to change task list ID, this is not permitted!");
+        }
+
+        TaskList existingTaskList = taskListRepository.findById(taskListId).orElseThrow(() -> new IllegalArgumentException("Task list not found!"));
+        existingTaskList.setTitle(taskList.getTitle());
+        existingTaskList.setDescription(taskList.getDescription());
+        existingTaskList.setUpdated(LocalDateTime.now());
+        return taskListRepository.save(existingTaskList);
+    }
+
+    @Override
+    public void deleteTaskList(UUID taskListId) {
+        taskListRepository.deleteById(taskListId);
     }
 }
